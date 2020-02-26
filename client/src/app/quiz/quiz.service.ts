dad6f0b6168abe35e6646dc555aebd0751880e1d
Sojisoyoye <soji.soyoye@gmail.com>
2020-02-26 22:16:34 +0100

    integrated angular

--- client/src/app/quiz/quiz.service.ts
+++ client/src/app/quiz/quiz.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/catch';
+import {API_URL} from '../env';
+import { Quiz } from './quiz.model';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private static _handleError(err: HttpErrorResponse | any) {
+    return Observable.throw(err.message || 'Error: Unable to complete request.');
+  }
 }
