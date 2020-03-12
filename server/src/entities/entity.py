dc4665582f7df51021dbcd89f4c73f7c379f03f7
Sojisoyoye <soji.soyoye@gmail.com>
2020-03-12 16:30:41 +0100

    secured app with auth0

--- server/src/entities/entity.py
+++ server/src/entities/entity.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 db_url = 'localhost:5432'
 db_name = 'quizzo'
 db_user = 'postgres'
-db_password = 'Olusoji@18'
+db_password = 'Olus'
 engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
 Session = sessionmaker(bind=engine)
 
